803ad61ec0bf4d9fa099fe98aed2c6995402f4d5
Maverick Seneris <mavseneris@gmail.com>
2023-01-08 13:40:18 +0800

    first commit

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+export default function Calculator() {
+  return (
+    <div className='calculator'>
+      <div className='container'>
+        <div className='display'>
+          12345
+        </div>
+        <div className='buttons'>
+          <button className='button'>C</button>
+          <button className='button'>()</button>
+          <button className='button'>%</button>
+          <button className='button'>/</button>
+          <button className='button'>7</button>
+          <button className='button'>8</button>
+          <button className='button'>9</button>
+          <button className='button'>x</button>
+          <button className='button'>4</button>
+          <button className='button'>5</button>
+          <button className='button'>6</button>
+          <button className='button'>-</button>
+          <button className='button'>1</button>
+          <button className='button'>2</button>
+          <button className='button'>3</button>
+          <button className='button'>+</button>
+          <button className='button'>+/-</button>
+          <button className='button'>0</button>
+          <button className='button'>.</button>
+          <button className='button'>=</button>
+
+        </div>
+      </div>
+    </div>
+  )
+}
